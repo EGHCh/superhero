f57c2935284a39c797e785831967a32f74c40808
EGHCh <63925946+EGHCh@users.noreply.github.com>
2022-04-01 19:34:28 -0300

    desafio completo

---
+++ assets/js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(() => {
+  const buscar = $("#buscar");
+  const input = $("#supeheroId");
+  const superHeroDisplay = $("#superhero-data");
+  const superHeroStats = $("#superhero-stats");
+
+  const SuperHeroImg = $("#superhero-details #superhero-img");
+  const Name = $("#superhero-details .name");
+  const Connections = $("#superhero-details .connections");
+  const Publisher = $("#superhero-details .publisher");
+  const FirstAppearance = $("#superhero-details .first-appearance");
+  const Height = $("#superhero-details .height");
+  const Weight = $("#superhero-details .weight");
+  const Aliases = $("#superhero-details .aliases");
+
+  buscar.click(() => {
+    const superId = input.val();
+    console.log(superId);
+    $.ajax(
+      `https://www.superheroapi.com/api.php/503190218081610/${superId}`
+    ).done((data) => {
+      SuperHeroImg.attr(`src`, data.image.url);
+      Name.html(data.name);
+      Connections.html(data.connections["group-affiliation"]);
+      Publisher.html(data.biography.publisher);
+      FirstAppearance.html(data.biography["first-appearance"]);
+      Height.html(data.appearance.height[1]);
+      Weight.html(data.appearance.weight[1]);
+      Aliases.html(data.biography.aliases);
+      
+      let options = {
+        title: {
+          text: "Desktop OS Market Share in 2017"
+        },
+        subtitles: [{
+          text: "As of November, 2017"
+        }],
+        animationEnabled: true,
+        data: [{
+          type: "pie",
+          startAngle: 40,
+          toolTipContent: "<b>{label}</b>: {y}%",
+          showInLegend: "true",
+          legendText: "{label}",
+          indexLabelFontSize: 16,
+          indexLabel: "{label} - {y}%",
+          dataPoints: [
+            { y: data.powerstats.intelligence, label: "Inteligencia" },
+            { y: data.powerstats.strength, label: "Fuerza" },
+            { y: data.powerstats.speed, label: "Velocidad" },
+            { y: data.powerstats.durability, label: "Resistenci" },
+            { y: data.powerstats.power, label: "Poder" },
+            { y: data.powerstats.combat, label: "Combate" },
+          ]
+        }]
+      };
+      $("#superhero-stats").CanvasJSChart(options);
+    });
+  });
+});
