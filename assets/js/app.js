3fd2db7a94f51f9343382176a9c6c9052eb3258b
EGHCh <63925946+EGHCh@users.noreply.github.com>
2022-04-02 14:20:10 -0300

    Corrección

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ $(document).ready(() => {
   buscar.click(() => {
     const superId = input.val();
     console.log(superId);
-    if(isNaN(superId)){
+    if (isNaN(superId)) {
       alert("No es un numero");
     }
     $.ajax(
@@ -30,32 +30,36 @@ $(document).ready(() => {
       Height.html(data.appearance.height[1]);
       Weight.html(data.appearance.weight[1]);
       Aliases.html(data.biography.aliases);
-      
+
       let options = {
         title: {
-          text: "Desktop OS Market Share in 2017"
+          text: "Desktop OS Market Share in 2017",
         },
-        subtitles: [{
-          text: "As of November, 2017"
-        }],
+        subtitles: [
+          {
+            text: "As of November, 2017",
+          },
+        ],
         animationEnabled: true,
-        data: [{
-          type: "pie",
-          startAngle: 40,
-          toolTipContent: "<b>{label}</b>: {y}%",
-          showInLegend: "true",
-          legendText: "{label}",
-          indexLabelFontSize: 16,
-          indexLabel: "{label} - {y}%",
-          dataPoints: [
-            { y: data.powerstats.intelligence, label: "Inteligencia" },
-            { y: data.powerstats.strength, label: "Fuerza" },
-            { y: data.powerstats.speed, label: "Velocidad" },
-            { y: data.powerstats.durability, label: "Resistenci" },
-            { y: data.powerstats.power, label: "Poder" },
-            { y: data.powerstats.combat, label: "Combate" },
-          ]
-        }]
+        data: [
+          {
+            type: "pie",
+            startAngle: 40,
+            toolTipContent: "<b>{label}</b>: {y}%",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}%",
+            dataPoints: [
+              { y: data.powerstats.intelligence, label: "Inteligencia" },
+              { y: data.powerstats.strength, label: "Fuerza" },
+              { y: data.powerstats.speed, label: "Velocidad" },
+              { y: data.powerstats.durability, label: "Resistenci" },
+              { y: data.powerstats.power, label: "Poder" },
+              { y: data.powerstats.combat, label: "Combate" },
+            ],
+          },
+        ],
       };
       $("#superhero-stats").CanvasJSChart(options);
     });
